97e0fe5db8e3801f35ec29e0c7edbd10b7456dbe
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2024-04-04 19:25:37 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
 sudo rm -rf /home/.brewuser
-
 sudo useradd brew -d /home/.brewuser
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 sudo chown -R brew:brew /home/linuxbrew
+
+sudo wget -P /usr/local/bin https://raw.githubusercontent.com/dnkmmr69420/brew-user/main/bin/brewsh
+sudo wget -P /usr/local/bin https://raw.githubusercontent.com/dnkmmr69420/brew-user/main/bin/ebrew
+sudo wget -P /usr/local/bin https://raw.githubusercontent.com/dnkmmr69420/brew-user/main/bin/sbrew
+
+sudo chmod +x /usr/local/bin/brewsh
+sudo chmod +x /usr/local/bin/ebrew
+sudo chmod +x /usr/local/bin/sbrew
