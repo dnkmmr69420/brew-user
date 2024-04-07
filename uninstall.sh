55bd6e79af3468fcdc19339cc5fef3e824a80da7
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2024-04-06 22:19:56 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -11,9 +11,9 @@ sudo rm -rf /home/.brewuser
 
 sudo userdel brew
 
-sudo rm -rf /usr/local/bin/brewsh
-sudo rm -rf /usr/local/bin/ebrew
-sudo rm -rr /usr/local/bin/sbrew
+sudo rm -f /home/linuxbrew/.linuxbrew/bin/brewsh
+sudo rm -f /home/linuxbrew/.linuxbrew/bin/ebrew
+sudo rm -f /home/linuxbrew/.linuxbrew/bin/sbrew
 
 if ! [ -f /usr/etc/profile.d/brew.sh ]; then
   sudo rm -rf /etc/profile.d/brew.sh
