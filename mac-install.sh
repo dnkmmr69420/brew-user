5eaaf1930634760d1fdece368cbdaa2950e04929
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2025-06-29 14:07:54 -0500

    Update mac-install.sh

--- mac-install.sh
+++ mac-install.sh
@@ -2,11 +2,11 @@
 
 # This only supports Apple Silicon. There will be no plans to make a port for Intel Macs.
 
-dscl . -create /Users/brewuser
-dscl . -create /Users/brewuser UserShell /bin/bash
-dscl . -create /Users/brewuser RealName "Homebrew User"
-dscl . -create /Users/brewuser UniqueID 510
-dscl . -create /Users/brewuser PrimaryGroupID 20
-dscl . -create /Users/brewuser NFSHomeDirectory /opt/brewuser
+dscl . -create /Users/brew
+dscl . -create /Users/brew UserShell /bin/bash
+dscl . -create /Users/brew RealName "Homebrew User"
+dscl . -create /Users/brew UniqueID 510
+dscl . -create /Users/brew PrimaryGroupID 20
+dscl . -create /Users/brew NFSHomeDirectory /opt/brewuser
 mkdir -p /opt/brewuser
 chown -R brewuser:staff /opt/brewuser
